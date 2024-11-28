2772a0542242d65f150a54f1ff47480ec81f3c09
Hyoan Moon <tjdud24as@gmail.com>
2024-11-28 00:49:10 -0500

    Implemented login function in client-server

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -25,5 +25,14 @@ function login() {
     body: JSON.stringify(req),
   })
     .then((res) => res.json())
-    .then(console.log);
+    .then((res) => {
+      if (res.success) {
+        location.href = "/";
+      } else {
+        alert(res.msg);
+      }
+    })
+    .catch((err) => {
+      console.error(new Error("An error occurred while trying to log in"));
+    });
 }
