0f81a30d793712ab851105f03dd92bf28a0998bc
Hyoan Moon <tjdud24as@gmail.com>
2024-11-27 19:46:15 -0500

    Create a fetch request for login

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,27 @@
 "use strict";
 
-console.log("Hello!");
-console.log("Hello!");
+const id = document.querySelector("#id");
+const pw = document.querySelector("#pw");
+const loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+  const req = {
+    id: id.value,
+    pw: pw.value,
+  };
+  //   console.log(req);
+  //   console.log("value", id.value);
+
+  console.log(req);
+  console.log(JSON.stringify(req));
+
+  fetch("/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req),
+  });
+}
