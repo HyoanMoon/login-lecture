593d05dbe21dc254738f2c23148c0db5bc26a7dc
Hyoan Moon <tjdud24as@gmail.com>
2024-12-05 02:15:33 -0500

    modify db.js file

--- app/src/config/db.js
+++ app/src/config/db.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-  host: "login-lecture.c9a8kyko6w5f.ap-northeast-2.rds.amazonaws.com",
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PW,
   database: process.env.DB_DATABASE,
