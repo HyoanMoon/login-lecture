100026e3abfaafd2e786f1452013b79af93796aa
Hyoan Moon <tjdud24as@gmail.com>
2024-12-09 22:55:48 -0500

    Implemented login function with final optimization

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -5,17 +5,17 @@ const User = require("../../models/User");
 
 const output = {
   home: (req, res) => {
-    logger.info(`GET / 200 "move to home page"`);
+    logger.info(`GET / 304 "move to home page"`);
     res.render("home/index");
   },
 
   login: (req, res) => {
-    logger.info(`GET /login 200 "move to login page"`);
+    logger.info(`GET /login 304 "move to login page"`);
     res.render("home/login");
   },
 
   register: (req, res) => {
-    logger.info(`GET /register 200 "move to register page"`);
+    logger.info(`GET /register 304 "move to register page"`);
     res.render("home/register");
   },
 };
@@ -24,34 +24,48 @@ const process = {
   login: async (req, res) => {
     const user = new User(req.body);
     const response = await user.login();
-    if (response.err)
-      logger.error(
-        `POST /login 200 Response: "success:${response.success}, ${response.err}"`
-      );
-    else
-      logger.info(
-        `POST /login 200 Response: "success:${response.success}, msg:${response.msg}"`
-      );
+
+    const url = {
+      method: "POST",
+      path: "/login",
+      status: response.err ? 400 : 200,
+    };
+
+    log(response, url);
     // console.log(response);
-    return res.json(response);
+    return res.status(url.status).json(response);
   },
 
   register: async (req, res) => {
     const user = new User(req.body);
     const response = await user.register();
-    if (response.err)
-      logger.error(
-        `POST /login 200 Response: "success:${response.success}, ${response.err}"`
-      );
-    else
-      logger.info(
-        `POST /register 200 Response: "success:${response.success}, msg:${response.msg}"`
-      );
-    return res.json(response);
+
+    const url = {
+      method: "POST",
+      path: "/register",
+      status: response.err ? 409 : 201,
+    };
+
+    log(response, url);
+    return res.status(url.status).json(response);
   },
 };
 
 module.exports = {
   output,
   process,
+};
+
+const log = (response, url) => {
+  if (response.err) {
+    logger.error(
+      `${url.method} ${url.path} ${url.status} Response: ${response.success}, ${response.err}`
+    );
+  } else {
+    logger.info(
+      `${url.method} ${url.path} ${url.status} Response: ${
+        response.success
+      }, msg: ${response.msg || ""}`
+    );
+  }
 };
